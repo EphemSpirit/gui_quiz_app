5fb3a5f1c7e4c41db603a6b0ea2b3251f42fd231
Drew Hund <languageinvestigator@gmail.com>
2026-04-28 14:58:54 -0400

    oop based UI

--- ui.py
+++ ui.py
@@ -1,2 +1,45 @@
+from tkinter import *
+from quiz_brain import QuizBrain
 THEME_COLOR = "#375362"
 
+class QuizInterface:
+
+    def __init__(self, quiz: QuizBrain):
+        self.quiz = quiz
+        self.window = Tk()
+        self.window.title("Quizzler")
+        self.window.config(padx=20, pady=20, bg=THEME_COLOR)
+
+        # SCORE LABEL
+        self.score_label = Label(text="Score: 0", fg="white", bg=THEME_COLOR)
+        self.score_label.grid(row=0, column=1)
+
+        # CANVAS
+        self.canvas = Canvas(width=300, height=250, bg="white")
+        self.question_text = self.canvas.create_text(
+            150,
+            125,
+            text="Question Text",
+            fill=THEME_COLOR,
+            font=("Ariel", 10, "italic"),
+            width=250
+        )
+        self.canvas.grid(row=1, column=0, columnspan=2, pady=50)
+
+        # TRUE/FALSE BUTTONS
+        true_img = PhotoImage(file="./images/true.png")
+        false_img = PhotoImage(file="./images/false.png")
+
+        self.true_btn = Button(image=true_img)
+        self.true_btn.grid(row=2, column=0)
+
+        self.false_btn = Button(image=false_img)
+        self.false_btn.grid(row=2, column=1)
+
+        self.get_next_question()
+
+        self.window.mainloop()
+
+    def get_next_question(self):
+        q_text = self.quiz.next_question()
+        self.canvas.itemconfig(self.question_text, text=q_text)
