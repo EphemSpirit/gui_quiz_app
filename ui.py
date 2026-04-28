d2b951afc3ff42fdd4d62838dd5233b758f6bf05
Drew Hund <languageinvestigator@gmail.com>
2026-04-28 15:22:40 -0400

    full gui quizk game

--- ui.py
+++ ui.py
@@ -30,10 +30,10 @@ class QuizInterface:
         true_img = PhotoImage(file="./images/true.png")
         false_img = PhotoImage(file="./images/false.png")
 
-        self.true_btn = Button(image=true_img)
+        self.true_btn = Button(image=true_img, command=self.answer_true)
         self.true_btn.grid(row=2, column=0)
 
-        self.false_btn = Button(image=false_img)
+        self.false_btn = Button(image=false_img, command=self.answer_false)
         self.false_btn.grid(row=2, column=1)
 
         self.get_next_question()
@@ -41,5 +41,29 @@ class QuizInterface:
         self.window.mainloop()
 
     def get_next_question(self):
-        q_text = self.quiz.next_question()
-        self.canvas.itemconfig(self.question_text, text=q_text)
+        self.canvas.config(bg="white")
+        if self.quiz.still_has_questions():
+            self.score_label.config(text=f"Score: {self.quiz.score}")
+            q_text = self.quiz.next_question()
+            self.canvas.itemconfig(self.question_text, text=q_text)
+        else:
+            self.canvas.itemconfig(self.question_text, text="Quiz finished!")
+            self.true_btn.config(state="disabled")
+            self.false_btn.config(state="disabled")
+
+    def answer_true(self):
+        is_right = self.quiz.check_answer("True")
+        self.give_feedback(is_right)
+
+    def answer_false(self):
+        is_right = self.quiz.check_answer("False")
+        self.give_feedback(is_right)
+
+    def give_feedback(self, is_right):
+        if is_right:
+            self.canvas.config(bg="green")
+        else:
+            self.canvas.config(bg="red")
+
+        self.window.after(1000, self.get_next_question)
+
